0b39c54e964c9b122d412300b3ee5bd73ffb1c09
ermias111 <ermimichael@gmail.com>
2019-10-25 15:11:00 +0300

    Single Page Applications Part 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+import 'hammerjs';
+
+import { AppComponent } from './app.component';
+import { DishService } from '../app/services/dish.service';
+import { PromotionService } from '../app/services/promotion.service';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    DishdetailComponent,
+    HomeComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule
+  ],
+  providers: [
+    DishService,
+    PromotionService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Promotion } from '../shared/promotion';
+import { PromotionService } from '../services/promotion.service';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  dishes: Dish[];
+  promotions: Promotion[];
+
+  constructor(private dishService: DishService, private promotionService: PromotionService) { }
+
+  ngOnInit() {
+    this.dishes = this.dishService.getDishes();
+    this.promotions = this.promotionService.getPromotions();
+  }
+
+}
