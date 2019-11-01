6db2c0b94f2d0facfa2c6bc76234feb1e6b3883d
ermias111 <ermimichael@gmail.com>
2019-11-01 11:38:46 +0300

    rxjs part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,9 +26,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     // this.dishes = this.dishService.getDishes();
     // this.promotions = this.promotionService.getPromotions();
-    this.dishService.getFeaturedDish().then((dish) => this.dish = dish);;
-    this.promotionService.getFeaturedPromotion().then((promo) => this.promotion = promo);;
-    this.leaderService.getFeaturedLeader().then((leader) => this.leader = leader);;
+    this.dishService.getFeaturedDish().subscribe((dish) => this.dish = dish);;
+    this.promotionService.getFeaturedPromotion().subscribe((promo) => this.promotion = promo);;
+    this.leaderService.getFeaturedLeader().subscribe((leader) => this.leader = leader);;
   }
 
 }
